2ae05da88b401ffc02d2de6891a8ac13bcfdd0ea
PrinceTheDev <sinbadprince9@gmail.com>
2024-08-28 19:44:40 +0100

    models setup to store weather request

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class LogWeatherRequest(models.Model):
+    city = models.CharField(max_length=100)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    response_data = models.JSONField()
+
+    def __str__(self):
+        return f"{self.city} at {self.timestamp}"
